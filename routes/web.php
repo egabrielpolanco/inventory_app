0f5d342a3fab47036f2eee18167d9250d0cc4205
Gabriel Polanco <egabriel.polanco@gmail.com>
2024-09-12 11:54:11 -0600

    delete fake routes

--- routes/web.php
+++ routes/web.php
@@ -24,13 +24,6 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/inventory/delete', [InventoryController::class, 'delete'])
     ->name('inventory.delete');
 
-   /* Route::get('libros/create', 'LibroController@create')->name('libros.create')->middleware('permission:libros.create');
-    Route::post('libros/store', 'LibroController@store')->name('libros.store')->middleware('permission:libros.create');
-    Route::get('libros/{libro}', 'LibroController@show')->name('libros.show')->middleware('permission:libros.show');
-    Route::get('libros/{libro}/edit', 'LibroController@edit')->name('libros.edit')->middleware('permission:libros.edit');
-    Route::put('libros/update', 'LibroController@update')->name('libros.update')->middleware('permission:libros.edit');
-    Route::delete('libros/{libro}', 'LibroController@destroy')->name('libros.delete')->middleware('permission:libros.delete');*/
-
 });
 
 
